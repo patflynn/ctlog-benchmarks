2b3f5e40abbbfe6605cf4c97860f548a25091e2a
Patrick Flynn <paflynn@google.com>
2026-01-28 19:12:21 +0000

    Add Terraform config and GHA workflow

---
+++ terraform/apis.tf
@@ -0,0 +1,16 @@
+resource "google_project_service" "apis" {
+  for_each = toset([
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "sqladmin.googleapis.com",
+    "spanner.googleapis.com",
+    "storage-api.googleapis.com"
+  ])
+
+  project            = var.project_id
+  service            = each.key
+  disable_on_destroy = false
+}
---
+++ terraform/workload_identity.tf
@@ -0,0 +1,47 @@
+# Workload Identity Pool
+resource "google_iam_workload_identity_pool" "github_pool" {
+  workload_identity_pool_id = "github-actions-pool"
+  display_name              = "GitHub Actions Pool"
+  description               = "Identity pool for GitHub Actions"
+  project                   = var.project_id
+}
+
+# Workload Identity Provider
+resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  display_name                       = "GitHub Provider"
+  description                        = "OIDC Provider for GitHub Actions"
+  project                            = var.project_id
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.repository" = "assertion.repository"
+  }
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+# Service Account for CI/CD
+resource "google_service_account" "ci_sa" {
+  account_id   = "github-actions-sa"
+  display_name = "GitHub Actions Service Account"
+  project      = var.project_id
+}
+
+# Allow GitHub Actions to impersonate the Service Account
+resource "google_service_account_iam_member" "workload_identity_user" {
+  service_account_id = google_service_account.ci_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${var.github_repo}"
+}
+
+# Grant necessary permissions to the Service Account (Broad for now to allow infra creation)
+resource "google_project_iam_member" "ci_sa_owner" {
+  project = var.project_id
+  role    = "roles/owner" # TODO: Scope this down later
+  member  = "serviceAccount:${google_service_account.ci_sa.email}"
+}
