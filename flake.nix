84d931715ee8ded2de0d449d2a809f9303f9ca1b
Patrick Flynn <paflynn@google.com>
2026-01-28 20:31:38 +0000

    Add Nix flake for dev environment

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix flake for CT Log Benchmarks development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true; # Required for terraform
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            terraform
+            google-cloud-sdk
+            git
+            gh
+            go
+          ];
+
+          shellHook = ''
+            echo "Welcome to the CT Log Benchmarks dev environment!"
+            echo "Terraform version: $(terraform version | head -n1)"
+            echo "GCloud version: $(gcloud --version | head -n1)"
+          '';
+        };
+      }
+    );
+}
